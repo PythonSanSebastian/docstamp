a8931d673e44bc565296602e4022a371361b258a
Alexandre M. S <alexsavio@gmail.com>
2015-03-27 17:19:12 +0100

    pdflatex.py: add xelatex function and cmd args as list

--- docstamp/pdflatex.py
+++ docstamp/pdflatex.py
@@ -10,11 +10,14 @@
 
 import os
 import shutil
+import logging
 import os.path      as      op
 from   glob         import  glob
 
-from   .commands    import  call_command
-from   .filenames   import  remove_ext
+from   .commands    import  call_command, simple_call
+from   .filenames   import  remove_ext, cleanup
+
+log = logging.getLogger(__name__)
 
 
 def tex2pdf(tex_file, output_file=None, output_format='pdf'):
@@ -45,19 +48,18 @@ def tex2pdf(tex_file, output_file=None, output_format='pdf'):
 
     cmd_name = 'pdflatex'
 
-    args_strings = ' '
+    args_strings = [cmd_name]
     if output_file is not None:
-        args_strings += '-output-directory={} '.format(op.abspath(op.dirname(output_file)))
+        args_strings += ['-output-directory="{}" '.format(op.abspath(op.dirname(output_file)))]
 
-    if output_file:
-        args_strings += '-output-format={} '.format(output_format)
-        result_dir    = op.dirname(output_file)
-    else:
-        result_dir    = op.dirname(tex_file)
+    result_dir = op.dirname(output_file) if output_file else op.dirname(tex_file)
 
-    args_strings += tex_file
+    args_strings += ['-output-format="{}"'.format(output_format)]
+    args_strings += ['"' + tex_file + '"']
 
-    ret = call_command(cmd_name, args_strings.split())
+    log.debug('Calling command {} with args: {}.'.format(cmd_name, args_strings))
+    #ret = call_command(cmd_name, args_strings)
+    ret = simple_call(args_strings)
 
     result_file = op.join(result_dir, remove_ext(op.basename(tex_file)) + '.' + output_format)
     if op.exists(result_file):
@@ -65,7 +67,62 @@ def tex2pdf(tex_file, output_file=None, output_format='pdf'):
     else:
         raise IOError('Could not find PDFLatex result file.')
 
-    [os.remove(f) for f in glob(op.join(result_dir, '*.aux'))]
-    [os.remove(f) for f in glob(op.join(result_dir, '*.log'))]
+    log.debug('Cleaning *.aux and *.log files from folder {}.'.format(result_dir))
+    cleanup(result_dir, 'aux')
+    cleanup(result_dir, 'log')
 
+    return ret
+
+
+def xetex2pdf(tex_file, output_file=None, output_format='pdf'):
+    """ Call XeLatex to convert TeX files to PDF.
+
+    Parameters
+    ----------
+    tex_file: str
+        Path to the input LateX file.
+
+    output_file: str
+        Path to the output PDF file.
+        If None, will use the same output directory as the tex_file.
+
+    output_format: str
+        Output file format. Choices: 'pdf' or 'dvi'. Default: 'pdf'
+
+    Returns
+    -------
+    return_value
+        XeLatex command call return value.
+    """
+    if not op.exists(tex_file):
+        raise IOError('Could not find file {}.'.format(tex_file))
+
+    if output_format != 'pdf' and output_format != 'dvi':
+        raise ValueError("Invalid output format given {}. Can only accept 'pdf' or 'dvi'.".format(output_format))
+
+    cmd_name = 'xelatex'
+
+    args_strings = [cmd_name]
+    if output_file is not None:
+        args_strings += ['-output-directory="{}"'.format(op.abspath(op.dirname(output_file)))]
+
+    if output_format == 'dvi':
+        args_strings += ['-no-pdf']
+
+    result_dir = op.dirname(output_file) if output_file else op.dirname(tex_file)
+    args_strings += ['"' + tex_file + '"']
+
+    log.debug('Calling command {} with args: {}.'.format(cmd_name, args_strings))
+    #ret = call_command(cmd_name, args_strings)
+    ret = simple_call(args_strings)
+
+    result_file = op.join(result_dir, remove_ext(op.basename(tex_file)) + '.pdf')
+    if op.exists(result_file):
+        shutil.move(result_file, output_file)
+    else:
+        raise IOError('Could not find PDFLatex result file.')
+
+    log.debug('Cleaning *.aux and *.log files from folder {}.'.format(result_dir))
+    cleanup(result_dir, 'aux')
+    cleanup(result_dir, 'log')
     return ret
